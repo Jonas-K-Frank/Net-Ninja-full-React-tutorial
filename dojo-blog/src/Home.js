83979fc20a92b6ec08deb2823c012033a8e77e4f
Jonas Frank <jonas@jkfrank.dk>
2022-08-06 13:43:27 +0200

    Hooks og custom hooks

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,18 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Bloglist from "./Bloglist";
+import useFetch from "./useFetch";
 
 const Home = () => {
-	// let name = "Mario";
-	const [name, setName] = useState("Mario");
-
-	const clickHandler = () => {
-		setName("Luigi");
-	};
+	const {
+		data: blogs,
+		isPending,
+		error,
+	} = useFetch("http://localhost:8000/blogs");
 
 	return (
 		<div className="home">
-			<h2>Homepage</h2>
-			<p>{name}</p>
-			<button onClick={clickHandler}>Click me</button>
+			{error && <div>{error}</div>}
+			{isPending && <div>Loading...</div>}
+			{blogs && <Bloglist blogs={blogs} title="All blogs" />}
 		</div>
 	);
 };
